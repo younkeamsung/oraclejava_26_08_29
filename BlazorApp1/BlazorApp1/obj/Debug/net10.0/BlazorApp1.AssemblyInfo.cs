4e753d6782ce91e88f793d6da56b947f5d195375
윤금성 <younsummer00@naver.com>
2026-09-12 18:01:38 +0900

    2026-09-12

--- BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc02baad121efbdc27dbfc3cc41641e2fca74a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7296725842c44fa85b519d08401358c100c030f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
